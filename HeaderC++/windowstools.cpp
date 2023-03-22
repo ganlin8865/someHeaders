fa7e7b9b825c1dce56501b09d80189b6770996aa
lin gan <LuGanlin8865@outlook.com>
2023-03-22 16:01:13 +0800

    添加了一个使用winapi生成真随机数的函数 Added a function to generate true random numbers using the winapi

--- HeaderC++/windowstools.cpp
+++ HeaderC++/windowstools.cpp
@@ -15,9 +15,13 @@ void sShowWindow(HWND hwnd)
 int RealRandom()
 {
     if (prov == NULL)
-            if (!CryptAcquireContext(&prov, NULL, NULL, PROV_RSA_FULL, CRYPT_SILENT | CRYPT_VERIFYCONTEXT))
-                ExitProcess(1);
-        int out;
-        CryptGenRandom(prov, sizeof(out), (BYTE*)(&out));
-        return out & 0x7fffffff;
+    {
+        if (!CryptAcquireContext(&prov, NULL, NULL, PROV_RSA_FULL, CRYPT_SILENT | CRYPT_VERIFYCONTEXT))
+        {
+            ExitProcess(1);
+        }         
+    }       
+    int out;
+    CryptGenRandom(prov, sizeof(out), (BYTE*)(&out));
+    return out & 0x7fffffff;
 }
