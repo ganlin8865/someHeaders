955900cf26bc9c1cd145e69aefc76e2bcb70b17c
lin gan <LuGanlin8865@outlook.com>
2023-03-24 15:27:36 +0800

    awa

--- HeaderC++/Main.cpp
+++ HeaderC++/Main.cpp
@@ -1,22 +1,51 @@
-//hello!
 #include "littletools.h"
 #include "windowstools.h"
 
-using namespace ltools::random;
-using namespace ltools::sort;
-using namespace ltools::stack;
+using namespace ltools;
 using namespace wintools::consle;
-using namespace wintools::msgbox;
-using namespace wintools::playtone;
-using namespace wintools::window;
+using namespace std;
+
+string Languege;
+
+void EnglishMain()
+{
+	cCls();
+	cout << "Hello! Welcome to the SomeHeaders tutorial program!";
+	cPause();
+	return;
+}
+
+void ChineseMain()
+{
+	cCls();
+	cout << "你好！欢迎查看SomeHeaders教程程序！";
+	cPause();
+	return;
+}
+
+void ProgramMain()
+{
+	cout << "Please Input A Languege:\(Chinese/English\)";
+	cin >> Languege;
+	if (Languege == "Chinese")
+	{
+		ChineseMain();
+		return;
+	}
+	else if (Languege == "English")
+	{
+		EnglishMain();
+		return;
+	}
+	else
+	{
+		throw runtime_error("X");
+		exit(1);
+	}
+}
 
 int main() 
 {
-	int a;
-	ltools::classic::print('f');
-	ltools::classic::print(1234);
-	ltools::classic::print(345.1);
-	ltools::classic::print("hee");
-	ltools::classic::input(a);
+	ProgramMain();
 	return 0;
 }
